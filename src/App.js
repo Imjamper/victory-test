5965fd5375c31529d67168b990f60e2b259084ac
loshkarev <loshkarev@elewise.com>
2017-07-19 14:51:57 +0400

    test commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { VictoryBar, VictoryAxis, VictoryZoomContainer, VictoryTooltip, VictoryChart, VictoryGroup, VictoryTheme } from 'victory';
+import { VictoryBar, VictoryAxis, VictoryZoomContainer, VictoryTooltip, VictoryChart, VictoryGroup, VictoryTheme, VictoryBrushContainer, Bar, VictoryLabel } from 'victory';
 import assign from 'object-assign';
 
 class App extends Component {
@@ -9,41 +9,45 @@ class App extends Component {
     super(props);
     this.state = {
       currentIndex: 0,
-      selectedDomain: null,
-      zoomDomain: null
+      selectedDomain: { x: [0, 6] },
+      zoomDomain: { x: [0, 6] },
+      data: []
     };
+    for (var i = 0; i < 10; i++){
+      this.state.data.push({
+        name: "Тест" + i,
+        duration: Math.random() * 1000,
+        color: '#D0F165',
+        durationText: '00:00',
+        label: "SomeTooltip"
+      });
+    }
   }
 
   handleZoom(domain) {
+    if (domain.x[1] > domain.x[0] + 6)
+      domain.x[1] = domain.x[0] + 6  
+    console.log("["+domain.x[0] + "," + domain.x[1]+"]");
+    console.log(domain);
     this.setState({selectedDomain: domain});
   }
 
   handleBrush(domain) {
+    
+    if (domain.x[1] > domain.x[0] + 6)
+      domain.x[1] = domain.x[0] + 6  
+    console.log("["+domain.x[0] + "," + domain.x[1]+"]");
+    console.log(domain);
     this.setState({zoomDomain: domain});
   }
 
-  render() {
-    const data = [
-                {
-                  name: "Инициатор",
-                  duration: 123123,
-                  color: '#D0F165',
-                  durationText:'00:00',
-                  label: "SomeTooltip"
-                }, {
-                  name: "Начальник",
-                  duration: 11111,
-                  color: '#A6D34B',
-                  durationText:'00:00',
-                  label: "SomeTooltip"
-                }, {
-                  name: "Бухгалтер",
-                  duration: 55554,
-                  durationText:'00:00',
-                  color: '#A7D54B',
-                  label: "SomeTooltip"
-                }
-              ];
+  handleMouseClick = (domain, event) => {
+    console.log(domain);
+    console.log(event);
+  }
+
+  render() {   
+    const data = this.state.data;    
     const tickValues = [
         123123,
         11111,
@@ -61,8 +65,9 @@ class App extends Component {
         <p className="App-intro">
           <div style={{height:'500px'}}>
               <VictoryChart
-                theme={VictoryTheme.material}
-                
+                theme={VictoryTheme.material} 
+                height={500}
+                width={500}               
                 containerComponent={
                   <VictoryZoomContainer
                     dimension="x"
@@ -70,43 +75,86 @@ class App extends Component {
                     onDomainChange={this.handleZoom.bind(this)}
                   />
                 } 
-                domainPadding={50}>
-                <VictoryBar data={data} x="name" y={(d) => d.duration}
-                  animate={{ onLoad: { duration: 1000 } }}
-                  
+               
+              >
+
+                <VictoryBar 
+                  data={data}
+                  x="name" 
+                  y={(d) => d.duration}
+                  animate={{ onLoad: { duration: 1000 } }}                  
                   style={{ data: 
-                  {
-                    fill:(d) => {
-                      return d.color;
-                    },
-                    width: 60 
-                  } 
+                    {
+                      fill:(d) => {
+                        return d.color;
+                      },
+                      width: 60 
+                    } 
                   }}
                   labelComponent={
-                  <VictoryTooltip
-                    cornerRadius={(d) => 0}
-                    pointerLength={(d) => 5}
-                    flyoutStyle={{
-                      fill: (d) => "white",
-                      stroke: (d) => '#d8dadf'
+                    <VictoryTooltip
+                      cornerRadius={(d) => 0}
+                      pointerLength={(d) => 5}
+                      flyoutStyle={{
+                        fill: (d) => "white",
+                        stroke: (d) => '#d8dadf'
+                      }}
+                    />
+                  }
+                  events={[
+                      {
+                        target: "data",
+                        eventHandlers: {
+                          onClick: () => {
+                            return [{
+                              target: "labels",
+                              mutation: (props) => {
+                                return console.log(props);
+                              }
+                            }];
+                          }
+                        }
+                      }
+                    ]}
+                />
+                <VictoryAxis dependentAxis tickFormat={(x) => { return "00:00"; }} style={{ axisLabel: { padding: 40 } }} />
+                <VictoryAxis style={{ axisLabel: { padding: 30 } }} />
+              </VictoryChart> 
+              </div>
+              <div style={{height:'100px'}}> 
+              <VictoryChart
+                theme={VictoryTheme.material} 
+                height={150}
+                width={600} 
+                domainPadding={{ x: [20, 20]}}
+               
+                containerComponent={
+                  <VictoryBrushContainer
+                    dimension="x"
+                    selectedDomain={this.state.selectedDomain}
+                    onDomainChange={this.handleBrush.bind(this)}
+                    responsive={false}
+                    viewBox = "0, 0, 100%, auto"
+                  />
+                }
+              >
+                  
+                  <VictoryBar 
+                    data={data} 
+                    y={(d) => d.duration}
+                    style={{ data: 
+                    {
+                      fill:(d) => {
+                        return d.color;
+                      },
+                      width: 10
+                      
+                    } 
                     }}
-                  />}
-                />
-              <VictoryAxis dependentAxis
-                  tickFormat={(x) => 
-                  {
-                      return "00:00";
-                  }}
-                  style={{
-                    axisLabel: { padding: 40 }
-                  }}
-                />
-              <VictoryAxis
-                style={{
-                  axisLabel: { padding: 30 }
-                }}
-              />
-              </VictoryChart>
+                    labelComponent={<VictoryLabel text=""/>}               
+                  />
+                  <VictoryAxis  />
+          </VictoryChart>
             </div>
         </p>
       </div>
